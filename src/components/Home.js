16fbe0bf1b611780ca9ca6ba12baf93a93feeef5
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-21 13:39:00 -0400

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,19 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+
+  const myColor = {
+    color: "firebrick"
+  };
+
+  return (
+    <div id="home">
+      <h1 style={myColor}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
+
 
 export default Home;
